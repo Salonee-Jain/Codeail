968216160ce3af67f947b4ef2f97ef0f74ed4b90
Salonee-Jain <salonijain1248@gmail.com>
2023-02-18 12:33:48 +0530

    made changes to home cantroller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,34 +2,32 @@ const Comment = require('../models/comment')
 const Post = require('../models/post');
 const User = require('../models/user')
 
-module.exports.home = function(req, res){
+module.exports.home = function (req, res) {
+
+    // Post.find({})
+    // .populate('user')
+    // .populate({
+    //     path:'comments',
+    //     populate:{
+    //         path: 'user'
+    //     }
+    // })
+    // .exec((err, posts)=>{
+    //     if(err){console.log("error")}
+    //     posts.reverse();
+    //     User.find({}, (err, users) => {
+    //         if (err) { console.log("error") }
+    //         return res.render('home', {
+    //             title: "Home",
+    //             posts: posts,
+    //             all_users: users,
+
+    //         });
+    //     })
+    // });
+
+
  
-
-
-    
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path: 'user'
-        }
-    })
-    .exec((err, posts)=>{
-        if(err){console.log("error")}
-        posts.reverse();
-        User.find({}, (err, users) => {
-            if (err) { console.log("error") }
-            return res.render('home', {
-                title: "Home",
-                posts: posts,
-                all_users: users,
-
-            });
-        })
-    });
-
-   
 }
 
 // module.exports.actionName = function(req, res){}
