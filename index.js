f5570d8391bd3727b639f09475441fbd01e058cd
Salonee-Jain <salonijain1248@gmail.com>
2023-03-31 10:03:54 +0530

    fixed views error

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.set('layout extractScripts', true);
 // set up the view engine
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '..', 'views'));
+app.set('views', path.join(__dirname, '.', 'views'));
 
 //session middleware with options
 app.use(session({
